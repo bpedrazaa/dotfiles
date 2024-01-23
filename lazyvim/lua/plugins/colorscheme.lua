a5038348984c9e512116485029fe1259f58e695b
bpedrazaa <bpedrazaa@gmail.com>
2024-01-23 09:43:06 -0400

    Again, add lazyvim conf

---
+++ lazyvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      italic = {
+        strings = false,
+        comments = false,
+      },
+      transparent_mode = true,
+      palette_overrides = {
+        neutral_red = "#fb4934",
+      },
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
