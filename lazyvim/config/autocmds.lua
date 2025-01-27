4d6f14628ba95c16c905db8290ed2ca775c3a3c6
bpedrazaa <bpedrazaa@gmail.com>
2025-01-27 15:04:39 -0400

    Add extra lazyvim configurations

--- lazyvim/config/autocmds.lua
+++ lazyvim/config/autocmds.lua
@@ -3,14 +3,18 @@
 -- Add any additional autocmds here
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  pattern = { "term://lazygit" },
-  callback = function()
-    vim.schedule(function()
-      -- Send keys `ze` to scroll left...
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>zei", true, true, true), "i", true)
-      -- ...and try (_try_) to disable horizontal scrolling in the buffer
-      vim.cmd("set sidescrolloff=" .. vim.o.columns)
-      vim.cmd("redraw!")
-    end)
-  end,
+	pattern = { "term://lazygit" },
+	callback = function()
+		vim.schedule(function()
+			-- Send keys `ze` to scroll left...
+			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>zei", true, true, true), "i", true)
+			-- ...and try (_try_) to disable horizontal scrolling in the buffer
+			vim.cmd("set sidescrolloff=" .. vim.o.columns)
+			vim.cmd("redraw!")
+		end)
+	end,
 })
+
+vim.cmd([[
+  autocmd BufReadPre * hi TrailingWhitespace guibg=#e06c75|match TrailingWhitespace /\s\+$/
+]])
